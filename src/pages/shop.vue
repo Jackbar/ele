46d08b39d33590afe0152af939f1a90c6ef45319
Jackbar <liuhuang@vip.163.com>
2017-03-09 17:22:50 +0800

    ‘新建商铺页’

---
+++ src/pages/shop.vue
@@ -0,0 +1,43 @@
+<template>
+
+</template>
+
+<style lang="less" scoped></style>
+
+<script lang="">
+  export default {
+    // Options / Data
+    // data () { return {} },
+    // props: [],
+    // propsData: {},
+    // computed: {},
+    // methods: {},
+    // watch: {},
+    // Options / DOM
+    // el () {},
+    // template: '',
+    // render () {},
+    // Options / Lifecycle Hooks
+    // beforeCreate () {},
+    // created () {},
+    // beforeMount () {},
+    // mounted () {},
+    // beforeUpdate () {},
+    // updated () {},
+    // activated () {},
+    // deactivated () {},
+    // beforeDestroy () {},
+    // destroyed () {},
+    // Options / Assets
+    // directives: {},
+    // filters: {},
+    // components: {},
+    // Options / Misc
+    // parent: null,
+    // mixins: [],
+    // name: '',
+    // extends: {},
+    // delimiters: [ '{{', '}}' ],
+    // functional: false
+  }
+</script>
