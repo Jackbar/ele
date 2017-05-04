8e0dd8608a1e081631381e96cbf39c48da43ffad
jackbar <liuhuang@vip.163.com>
2017-05-05 00:24:44 +0800

    增加路由异步加载，px2rem

--- src/pages/msite.vue
+++ src/pages/msite.vue
@@ -1,79 +1,85 @@
 <template>
-<div class="wrapper">
-  <div class="header"></div>
-  <div class="foodentry-wrapper"></div>
-  <h3 class="index-title">推荐商家</h3>
-  <section class="shoplist" v-infinite-scroll="loadMore" infinite-scroll-disabled='busy' infinite-scroll-distance='60' infinite-scroll-immediate-check='false'>
-    <shoplist :shop='item' v-for='item in shoplist'></shoplist>
-  </section>
+  <div class="wrapper">
+    <div class="header"></div>
+    <div class="foodentry-wrapper"></div>
+    <h3 class="index-title">推荐商家</h3>
+    <section class="shoplist" v-infinite-scroll="loadMore" infinite-scroll-disabled='busy' infinite-scroll-distance='60'
+             infinite-scroll-immediate-check='false'>
+      <shoplist :shop='item' v-for='item in shoplist'></shoplist>
+    </section>
 
-</div>
+  </div>
 </template>
 
 <script>
-import fetchData from '../common/js/fetchData.js'
-import shoplist from '../components/shoplist/index.vue'
-export default {
-  name: 'hello',
-  data() {
-    return {
-      shoplist: [],
-      busy: false,
-      loadedComputed: false,
-      offset: 0,
-      limit: 20
-    }
-  },
-  components: {
-    shoplist
-  },
-  methods: {
-    loadMore() {
-      var self = this;
-      if (!this.loadedComputed) {
-        this.busy = true;
-        fetchData.shoplist({latitude:22.572909,longitude:113.86192,offset:this.offset,limit:this.limit}).then(res => {
-          if (res.length < self.limit) {
-            self.loadedComputed = true;
-          }
-          self.shoplist = self.shoplist.concat(res);
-          self.offset += self.limit;
-          self.busy = false;
-        })
+  import fetchData from '../common/js/fetchData.js'
+  import shoplist from '../components/shoplist/index.vue'
+  export default {
+    name: 'hello',
+    data() {
+      return {
+        shoplist: [],
+        busy: false,
+        loadedComputed: false,
+        offset: 0,
+        limit: 20
       }
     },
-    goToShop() {
+    components: {
+      shoplist
+    },
+    methods: {
+      loadMore() {
+        var self = this;
+        if (!this.loadedComputed) {
+          this.busy = true;
+          fetchData.shoplist({
+            latitude: 22.572909,
+            longitude: 113.86192,
+            offset: this.offset,
+            limit: this.limit
+          }).then(res => {
+            if (res.length < self.limit) {
+              self.loadedComputed = true;
+            }
+            self.shoplist = self.shoplist.concat(res);
+            self.offset += self.limit;
+            self.busy = false;
+          })
+        }
+      },
+      goToShop() {
 
+      }
+    },
+    created() {
+      this.loadMore()
     }
-  },
-  created() {
-    this.loadMore()
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang='less' scoped>
-.wrapper {
+  .wrapper {
     .header {
-        width: 100%;
-        height: 250px;
-        background-color: #0096ff;
+      width: 100%;
+      height: 250px;
+      background-color: #0096ff;
     }
     .foodentry-wrapper {
-        min-height: 354px;
-        border-bottom: 1px solid #eee;
-        background-color: #fff;
+      min-height: 354px;
+      border-bottom: 1px solid #eee;
+      background-color: #fff;
     }
     .index-title {
-        margin-top: 20px;
-        line-height: 68px;
-        font-weight: 600;
-        background-color: #fff;
-        border-top: 1px solid #eee;
-        border-bottom: 1px solid #eee;
-        font-size: 30px;
-        padding-left: 30px;
+      margin-top: 20px;
+      line-height: 68px;
+      font-weight: 600;
+      background-color: #fff;
+      border-top: 1px solid #eee;
+      border-bottom: 1px solid #eee;
+      font-size: 30px;
+      padding-left: 30px;
     }
-}
+  }
 </style>
